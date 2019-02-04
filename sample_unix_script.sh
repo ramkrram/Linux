e58979cfcbf280db1cf96820cfa9fbe3330af346
ramkrram <42908772+ramkrram@users.noreply.github.com>
2019-02-05 00:42:37 +0530

    Unix Script added

---
+++ sample_unix_script.sh
@@ -0,0 +1,7 @@
+echo "---------------------------------- First pipeline job creation in Azure Devops in UNIX-------------------------------------"
+
+echo "\n\n\t List doen all running processes"
+ps -ef
+ 
+ 
+echo "---------------------------------- END of Report -------------------------------------"
